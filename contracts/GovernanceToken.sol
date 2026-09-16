8c4ba83e464799b73a01cd43b3a9e2cbb9f1f09a
SADANAND <thesadanand.toponed0@gmail.com>
2026-09-16 17:53:24 +0530

    refactor: keep governance token fixed-supply and ERC20Votes based

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,38 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {NoncesUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
-contract GovernanceToken is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20PermitUpgradeable,
-    ERC20VotesUpgradeable,
-    OwnableUpgradeable,
-    UUPSUpgradeable
-{
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address initialOwner, uint256 initialSupply) external initializer {
-        require(initialOwner != address(0), "GovernanceToken: zero owner");
-        __ERC20_init("Task Governance Token", "TGT");
-        __ERC20Permit_init("Task Governance Token");
-        __ERC20Votes_init();
-        __Ownable_init(initialOwner);
-        _mint(initialOwner, initialSupply);
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor(uint256 initialSupply)
+        ERC20("Task Governance Token", "TGT")
+        ERC20Permit("Task Governance Token")
+    {
+        _mint(msg.sender, initialSupply);
     }
 
     function _update(address from, address to, uint256 value)
         internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+        override(ERC20, ERC20Votes)
     {
         super._update(from, to, value);
     }
@@ -40,15 +24,9 @@ contract GovernanceToken is
     function nonces(address owner)
         public
         view
-        override(ERC20PermitUpgradeable, NoncesUpgradeable)
+        override(ERC20Permit, Nonces)
         returns (uint256)
     {
         return super.nonces(owner);
     }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        override
-        onlyOwner
-    {}
 }
