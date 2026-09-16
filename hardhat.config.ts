a365a80d13eb79aac60f07c2f9c037e9e8677053
SADANAND <thesadanand.toponed0@gmail.com>
2026-09-16 17:43:13 +0530

    chore: add project configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: process.env.RPC_URL || "http://127.0.0.1:8545",
+    },
+    hardhat: {
+      mining: {
+        auto: true,
+      },
+    },
+  },
+};
+
+export default config;
